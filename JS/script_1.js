3f217259f9575674210f7923ca6a9f9287f57c77
Maksym-Zyza <mmaksymziza@gmail.com>
2020-08-15 18:18:13 +0300

    МЕТОДЫ+МАТ. ФУНКЦИИ

--- JS/script_1.js
+++ JS/script_1.js
@@ -1,2 +1,135 @@
-const a = 5;
-console.log(a);
+// const a = 5;
+// console.log(a);
+// console.log(typeof a);
+
+// МЕТОДЫ:
+
+// -- Вывод пользователя - confirm() и promnt()
+
+// const sholdRenrw = confirm("Хотите продолжить подписку?");
+// console.log(sholdRenrw);
+
+// -- Введите числа Number(n)
+
+// let quantity = prompt("Введите кокличество товара?");
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
+
+// -- Парсинг числа: Number.parseInt() и Number.parseFloat()
+
+// let elementWhids = "50.2533px";
+// const resalt = Number.parseFloat(elementWhids);
+// console.log(resalt);
+
+// -- Округление числа - .toFixed(n)
+
+// let salary = 1300.87811;
+// console.log(Number(salary.toFixed(2)));
+// // console.log(salary);
+// // salary = Number(salary);
+
+// -- Приведение (приобразование) к числу:
+
+// const qwqw = "Hello";
+// console.log(Number(true));
+// console.log(Number(false));
+// console.log(Number(qwqw));
+
+// -- Математические функции:
+
+// console.log(Math);
+
+// Корень квадратный Math.sqrt()
+// console.log(Math.sqrt(9, 2));
+
+// // Возведение в степень - Math.pow()
+// // const result = Math.pow(2, 8);
+// console.log(2 ** 10);
+
+// const base = 2;
+// const power = 10;
+// console.log(base ** power);
+
+// -- Скрипт - Возведение в числа в степень
+
+// // 1.Попросить ввести число и сохранить переменную
+// let base = prompt("Введите число");
+// base = Number(base);
+// console.log(base);
+// // 2.Попросить ввести степень и сохранить переменную
+// let power = prompt("Введите степень");
+// power = Number(power);
+// console.log(power);
+// // 3.Возвести число в степень и вывести в консоль
+// console.log(base ** power);
+
+// -- Генерим псевдослучайные числа:
+// Math.random(); - генерит рандомное чило от 0 до 1
+// Math.round(); - округление целого чила
+
+// console.log(Math.random());
+// const max = 50;
+// const min = 1;
+// let result = Math.round(Math.random() * (max - min) + min);
+// console.log(result);
+
+// -- Рандомный цвет backgroundColor
+// const collors = ["orange", "blue", "tomato", "gray", "green", "blue"];
+// const max = collors.length - 1;
+// const min = 0;
+// const index = Math.round(Math.random() * (max - min) + min);
+// const collor = collors[index];
+// console.log(collor);
+// document.body.style.backgroundColor = collor;
+
+// -- Длина строки - свойсто length
+// const messege = "Узнать количество симоылов в стороке - 41";
+// console.log(messege.length);
+
+// -- Конкатенация строе (сшивание)
+// const firstName = "Donald";
+// const lactName = "Dack";
+// console.log(firstName + " " + secondName);
+
+// -- Скрипт который вывеедет строку в формате:
+// "Гость х у посесяется в z номер q"
+// const firstName = "Бил";
+// const lactName = "Джон";
+// const room = "716";
+// const type = "VIP";
+// console.log(
+//   firstName + " " + lactName + " поселяется в " + type + " № " + room
+// );
+
+// -- Шаблонные строки
+// console.log(`${firstName} ${lactName} поселяется в ${type} № ${room}`);
+
+// -- Нормализация с методом toLowerCase()
+// let brand = prompt("Введите бренд");
+// brand = brand.toLowerCase();
+// console.log(brand);
+
+// Первую букву не изменняем и применяем toLowerCase() со второго символа (буквы)
+// let brand = "SamSUgG";
+// brand = brand[0] + brand.slice(1).toLowerCase();
+// console.log(brand);
+
+// -- Поиск в строке с методом includes() и toLowerCase()
+const blaclist1 = "спам";
+const blaclist2 = "распродажа";
+
+const string1 = "Привет, я принц Абдул, это не спам";
+const string2 = "Самая большая РАСПРОДАЖА";
+const string3 = "Реальное предложение";
+
+console.log(string1.includes(blaclist1));
+console.log(string1.includes(blaclist2));
+
+console.log(string2.includes(blaclist1));
+console.log(string2.toLowerCase().includes(blaclist2));
+
+console.log(string3.includes(blaclist1));
+console.log(string3.includes(blaclist2));
+
+// -- ОПЕРАТОРЫ СРАВНЕНИЯ
--- README.md
+++ README.md
@@ -1,1 +1,76 @@
 # goit-js-hw-01
+
+1. pryttierrs.json
+
+2. Модули - type="module"
+   При подключениии файлов JS указываем type="module" чтобы в каждом файле можна было задавать одинаковые переменные.
+    <script src="./JS/script_1.js" type="module"></script>
+
+3. ПЕРЕМЕННЫЕ
+
+- const a = 5; //задаем переменную
+- console.log(a); //выводим в консоль, интерпритатор ищет значение снизу вверх!
+- console.log(typeof a); //проверяем тип (или довабить в задании "typeof")
+- alert('qwqw'); //выводит значение в окно
+
+4. МЕТОДЫ:
+   // -- Вывод пользователю - confirm() и promnt()
+
+- const sholdRenrw = confirm("Хотите продолжить подписку?");
+  console.log(sholdRenrw); - возвращает true или false блокирует исполнение кода
+- const quantity = prompt("Введите покличество товара?");
+  console.log(quantity); - возвращает введенное значение либо null (значение стррока)
+  quantity = Number(quantity); - для преобразования в числовое значение
+
+// -- Парсинг числа:
+Number.parseInt() и Number.parseFloat()
+
+let elementWhids = "50.2533px";
+const resalt = Number.parseFloat(elementWhids);
+console.log(resalt);
+
+- Number.parseInt() - вычисляет число до первого знака.
+- Number.parseFloat() - вычисляет нецелое число - "502.33"
+
+// -- Округление числа - число.toFixed(значение)
+
+let salary = 1300.87854;
+salary = salary.toFixed(2);
+console.log(salary);
+salary = Number(salary);
+
+Краткая запись: // значение выщитывается справа на лево
+let salary = 1300.87811;
+console.log(Number(salary.toFixed(2)));
+
+// -- Приведение (приобразование) к числу:
+
+console.log(Number(true)); // 1
+console.log(Number(false)); //0
+console.log(Number(qwqw)); // NaN
+
+5. МАТЕМАТИЧЕСКИЕ ФУНКЦИИ:
+   console.log(Math);
+
+// -- Возведение в степень: 2 в 8 степень:
+const base = 2;
+const power = 8;
+console.log(base \*\* power);
+const result = Math.pow(2, 8); //256 - Math.pow() - возведение в степень: 2 в 8 степени.
+console.log(result);
+
+<!-- console.log(2 ** 8); - современный аналог -->
+
+// -- Корень квадратный Math.sqrt()
+console.log(Math.sqrt(9, 2));
+
+// -- Скрипт -возведения числа в степень
+// -- Генерим псевдослучайные числа:
+// -- Рандомный цвет backgroundColor
+// -- Длина строки - свойсто length
+// -- Конкатенация строе (сшивание)
+// -- Скрипт который вывеедет строку в формате:
+// -- Шаблонные строки
+// -- Поиск в строке с методом includes() и toLowerCase()
+
+// -- ОПЕРАТОРЫ СРАВНЕНИЯ
