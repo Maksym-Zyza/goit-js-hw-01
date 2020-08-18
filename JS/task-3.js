b8bba04a8c6d0f05ff4ae7fc4a941226abb9ce73
Maksym-Zyza <mmaksymziza@gmail.com>
2020-08-18 10:03:30 +0300

    Задача 3_

--- JS/task-3.js
+++ JS/task-3.js
@@ -6,16 +6,14 @@
 // В противном случае, то есть если ни одно из предыдущих условий не выполнилось, записать в message строку 'Доступ запрещен, неверный пароль!'
 // После всех проверок вывести в alert значение переменной message.
 
-const ADMIN_PASSWORD = "jqueryismyjam";
+const ADMIN_PASSWORD = prompt(`Введите пароль`);
 let message;
 
-const password = prompt(`ADMIN PASSWORD`);
-
-if (ADMIN_PASSWORD !== false) {
-  message = "Отменено пользователем!";
-} else if (ADMIN_PASSWORD === true) {
+if (ADMIN_PASSWORD === "jqueryismyjam") {
   message = "Добро пожаловать!";
+} else if (ADMIN_PASSWORD === "" || ADMIN_PASSWORD === null) {
+  message = "Отменено пользователем!";
 } else {
   message = "Доступ запрещен, неверный пароль!";
 }
-console.log(message);
+alert(message);
